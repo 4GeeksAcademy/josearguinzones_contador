66cee1fd848389e8351b52ff1d108750cd699c76
JoseArguinzones <145572262+JoseArguinzones@users.noreply.github.com>
2023-11-27 14:09:09 +0000

    contador

--- src/js/index.js
+++ src/js/index.js
@@ -8,5 +8,25 @@ import "../styles/index.css";
 //import your own components
 import Home from "./component/home.jsx";
 
+let unidad=0;
+let decimales=0;
+let centenas=0;
+let miles=0;
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+setInterval(() => {
+    unidad++;
+    if(unidad>9){
+        unidad=0;
+        decimales++;
+    }
+    if(decimales>9){
+        decimales=0;
+        centenas++;
+    }
+    if(centenas>9){
+        centenas=0;
+        miles++;
+    }
+    ReactDOM.render(<Home unidad={unidad} decimales={decimales} centenas={centenas} miles={miles}/>, document.querySelector("#app"));
+}, 1000);
+
